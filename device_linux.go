e99fe9bddd9c53d3394b100655b2a0c5bc217df2
Abhishek <abhishek@smartclean.sg>
2021-06-22 11:57:22 +0800

    device linux

--- device_linux.go
+++ device_linux.go
@@ -4,8 +4,8 @@ import (
 	"encoding/binary"
 	"net"
 
-	"github.com/paypal/gatt/linux"
-	"github.com/paypal/gatt/linux/cmd"
+	"github.com/smartclean/gatt/linux"
+	"github.com/smartclean/gatt/linux/cmd"
 )
 
 type device struct {
