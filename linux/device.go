03927ffb81ca89fc494d4dbe1f2adac9b085a4d9
Abhishek <abhishek@smartclean.sg>
2021-06-22 11:53:37 +0800

    update lib

--- linux/device.go
+++ linux/device.go
@@ -7,8 +7,8 @@ import (
 	"syscall"
 	"unsafe"
 
-	"github.com/paypal/gatt/linux/gioctl"
-	"github.com/paypal/gatt/linux/socket"
+	"github.com/smartclean/gatt/linux/gioctl"
+	"github.com/smartclean/gatt/linux/socket"
 )
 
 type device struct {
--- linux/hci.go
+++ linux/hci.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"sync"
 
-	"github.com/paypal/gatt/linux/cmd"
-	"github.com/paypal/gatt/linux/evt"
+	"github.com/smartclean/gatt/linux/cmd"
+	"github.com/smartclean/gatt/linux/evt"
 )
 
 type HCI struct {
